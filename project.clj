2b49f232a435a9ba147dcdad4472533d33ffcec6
Shashy Dass <ShashyD@gmail.com>
2014-07-03 18:11:06 -0400

    dependencies

--- project.clj
+++ project.clj
@@ -5,4 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/java.jdbc "0.3.4"]
-                 [joda-time "2.3"]])
+                 [joda-time "2.3"]]
+  :profiles {:dev {:dependencies [[com.zaxxer/HikariCP "1.4.0"]
+                                  [com.impossibl.pgjdbc-ng/pgjdbc-ng "0.3"]
+                                  [com.h2database/h2 "1.4.179"]]
+                   :plugins []}})

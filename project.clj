5f4474b90d2e14dac69c5c5251e433419ac1f12b
Shashy Dass <ShashyD@gmail.com>
2014-07-02 23:11:18 -0400

    update dependencies, remove unneeded

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/java.jdbc "0.3.2"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/java.jdbc "0.3.4"]
                  [joda-time "2.3"]])
